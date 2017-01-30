80b8c7380d31caeff3b680b405d413c260de58b3
franco83-2 <franco.84@gmail.com>
2017-01-30 11:59:35 -0500

    fixed modal, need to add form variables

--- index.js
+++ index.js
@@ -235,25 +235,25 @@ function diffVal(){
 // }
 
 $(document).ready(function(){
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal({
-     dismissible: false, // Modal can be dismissed by clicking outside of the modal
-     opacity: .5, // Opacity of modal background
-     inDuration: 300, // Transition in duration
-     outDuration: 200, // Transition out duration
-     startingTop: '4%', // Starting top style attribute
-     endingTop: '10%', // Ending top style attribute
-     ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-      //  alert("Ready");
-       console.log(modal, trigger);
-     },
-    //  complete: function() { alert('Closed'); } // Callback for Modal close
-   }
- );
   $('.modal-btn').on('click', function(){
     $('.modal').css("visibility", "")
   })
-});
+
+  $('.modal').modal({
+     dismissible: false,
+     opacity: .5,
+     inDuration: 300,
+     outDuration: 200,
+     startingTop: '4%',
+     endingTop: '10%',
+     complete: function() {
+       $('.intro').remove()
+       $('.modal').remove()
+       runner();
+      }
+    }
+    )
+  });
 
 
-$(document).ready(runner)
+// $(document).ready(runner)
